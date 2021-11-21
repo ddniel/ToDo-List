558c6bb68e0ebda428573a3a4a6b61dc0b8997de
Danny Becerra <ddnielbec@outlook.com>
2021-11-21 17:58:34 -0500

    Agrego 2 listas independientes y footer y header genericos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = []; //En JavaScript es mejor usar let en vez de var, excepto en casos especificos.
-
+let workItems = [];//Aca se guardan los items de la lista Work
 app.set('view engine', 'ejs'); //Siempre despues de express() caso contrario la app todavia no existe
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,19 +22,29 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("es-EC", options); //Convierte la fecha a una fecha local con las opciones de la variable anterior
 
-  res.render("list", {KindOfDay: day, newListItems: items}); //Busca el archivo list dentro de la carpeta VIEWS y luego se asigna el valor de day a la variable kindOfDay para poder usar en el archivo .ejs
+  res.render("list", {listTitle: day, newListItems: items}); //Busca el archivo list dentro de la carpeta VIEWS y luego se asigna el valor de day a la variable kindOfDay para poder usar en el archivo .ejs
 });
 
 
 app.post("/", function(req, res){
-  item = req.body.newItem;
+  let item = req.body.newItem;
 
-  items.push(item); //Agrega infromacion a un array, en este caso lo que contiene item se agrega a items que es un array.
-
-  res.redirect("/")  //Redirecciona esto a app.get
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work") //redirecciona a /work
+  } else {
+    items.push(item); //Agrega infromacion a un array, en este caso lo que contiene item se agrega a items que es un array.
+    res.redirect("/");  //Redirecciona al directorio principal
+  }
 })
 
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
 
+app.get("/about", function(req, res){
+  res.render("about");
+})
 
 
 
